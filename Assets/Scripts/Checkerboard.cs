81e30ed0a5438a76a5ea9d5786b926b58b383f6f
miestl <loic.miest@epitech.eu>
2016-10-25 23:49:40 +0200

    Je sais pas comment cette UI fonctionne -Alex

---
+++ Assets/Scripts/Checkerboard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkerboard : MonoBehaviour {
+
+	public GameObject Intersection;
+
+	int posX;
+	int posY;
+	Pawn[][] pawnList;
+
+		void Start () {
+
+		Intersection.GetComponent<Pawn> ();
+			
+			pawnList = new Pawn[18][];
+			for (int i = 0; i < 18; i++) 
+			{
+				pawnList [i] = new Pawn[18];
+				for (int a = 0; a < 18; a++) 
+				{
+					Pawn tmp = (Pawn)Instantiate (Intersection.GetComponent<Pawn>(), new Vector3 (i, a, -5), Quaternion.identity);
+					pawnList[i][a] = tmp;
+				}
+			}
+			pawnList [1] [1].SetColor (Board.e_cell.Black);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
