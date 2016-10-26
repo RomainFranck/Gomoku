95d662a0c9eb0f5f4a622eefddce386223a571e3
miestl <loic.miest@epitech.eu>
2016-10-26 06:31:04 +0200

    Menu , Fade and some other stuff

---
+++ Assets/Scripts/ScriptMenu/DisplayOverColor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayOverColor : MonoBehaviour {
+
+	// Use this for initialization
+	Text col;
+	void Start () {
+
+		col = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void pd()
+	{
+		col.color = Color.green;
+		//sound
+	}
+
+	public void NotOver()
+	{
+		col.color = Color.black;
+	}
+}
