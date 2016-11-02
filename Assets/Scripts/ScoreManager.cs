9400420cbbdb9d1394798bc35ea80001cd897fea
sejour-a <sejour_a@jormungandr.eu>
2016-11-02 01:44:39 +0100

    Score system, MUST BE ADDED TO SCENE

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int PlayerNumber;
+	private Text TXT;
+
+	// Use this for initialization
+	void Start () {
+		TXT = GameObject.Find("Player" + PlayerNumber + "Score").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (PlayerNumber) {
+		case 1:
+			TXT.text = /*"Captured pawns: " + */Arbiter.Instance.player1.capturedPawns.ToString();
+			break;
+		
+		case 2:
+			TXT.text = /*"Captured pawns: " + */Arbiter.Instance.player2.capturedPawns.ToString();
+			break;
+		}
+	}
+}
